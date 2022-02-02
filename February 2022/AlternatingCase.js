a168b7b8074b6abed4330bf07cfb45570eb0a757
robbins-jake <robbinsjake.t@gmail.com>
2022-02-01 22:52:17 -0500

    feb entries

---
+++ February 2022/AlternatingCase.js
@@ -0,0 +1,8 @@
+//Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase.
+
+
+String.prototype.toAlternatingCase = function () {
+    return this.split('').map(a => a == a.toUpperCase() ? a.toLowerCase() : a.toUpperCase()).join('')
+}
+
+console.log("HeLLo WoRLD".toAlternatingCase())
