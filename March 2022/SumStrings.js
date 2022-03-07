6f911f097958f97b16b51d4fdf21154d075b306f
robbins-jake <robbinsjake.t@gmail.com>
2022-03-07 00:41:11 -0500

    march entries

---
+++ March 2022/SumStrings.js
@@ -0,0 +1,31 @@
+/* Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+
+Example: (Input1, Input2 -->Output)
+
+"4",  "5" --> "9"
+"34", "5" --> "39"
+"", "" --> "0"
+"2", "" --> "2"
+"-5", "3" --> "-2"
+
+Notes:
+
+    If either input is an empty string, consider it as zero.
+
+    Inputs and the expected output will never exceed the signed 32-bit integer limit (2^31 - 1) */
+
+function sumStr(a,b) {
+    let num1 = Number(a)
+    let num2 = Number(b)
+    num1 === '' ? 0 : num1
+    num2 === '' ? 0 : num2
+    return JSON.stringify(num1 + num2)
+}
+
+console.log(sumStr("4","5"))
+
+//Refactoring for simplicity
+
+function sumStr(a,b) {
+    return String(Number(a)+Number(b));
+  }
