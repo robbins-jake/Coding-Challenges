1dc861d5a4faf7c00154dd9b2231b215b0cae7f9
robbins-jake <robbinsjake.t@gmail.com>
2022-04-15 21:25:06 -0400

    Add new challenge

---
+++ May 2022/ConvertReversed.js
@@ -0,0 +1,5 @@
+// Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+function digitize(n) {
+  return n.toString().split('').map((num) => Number(num)).reverse();
+}
