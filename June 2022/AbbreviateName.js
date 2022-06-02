b7fe8c3b31dc3ead283dc8bdd1efaabeaaa2d3b1
robbins-jake <robbinsjake.t@gmail.com>
2022-06-01 23:13:56 -0400

    june entries

---
+++ June 2022/AbbreviateName.js
@@ -0,0 +1,9 @@
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+// The output should be two capital letters with a dot separating them.
+
+function abbrevName(name){
+    let arr = name.split(' ');
+    return arr[0][0].toUpperCase() + '.' + arr[1][0].toUpperCase()
+  
+  }
