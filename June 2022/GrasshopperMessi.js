b6d76f0be931fcb1c3e1b5f75cca001525bd4865
robbins-jake <robbinsjake.t@gmail.com>
2022-06-19 16:39:21 -0400

    june entries

---
+++ June 2022/GrasshopperMessi.js
@@ -0,0 +1,13 @@
+/* Messi is a soccer player with goals in three leagues:
+
+    LaLiga
+    Copa del Rey
+    Champions
+
+Complete the function to return his total number of goals in all three leagues. */
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals
+  }
+
+console.log(goals(0,0,0))
