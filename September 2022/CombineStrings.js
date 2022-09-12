ae8bf2920f285b6ea16b69300fde5e6995d803bb
robbins-jake <robbinsjake.t@gmail.com>
2022-09-11 21:14:12 -0400

    sept entries

---
+++ September 2022/CombineStrings.js
@@ -0,0 +1,5 @@
+// Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
+
+function combineNames(a,b) { 
+    return a + ' ' + b
+  }
