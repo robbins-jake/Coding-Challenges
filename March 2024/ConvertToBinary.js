a3adfa63850217dec124f8de4fafe274069cb1b4
Jake Robbins <robbinsjake.t@gmail.com>
2024-03-22 23:25:30 -0400

    mar entries

---
+++ March 2024/ConvertToBinary.js
@@ -0,0 +1,5 @@
+//Given a non-negative integer b, write a function which returns an integer d such that 
+//the binary representation of b is the same as the decimal representation of d.
+
+
+let toBinary = n => +(n.toString(2))
