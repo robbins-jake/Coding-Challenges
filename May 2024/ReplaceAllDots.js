65305ddfc83708223fb500850d7a935eed624d15
Jake Robbins <robbinsjake.t@gmail.com>
2024-05-08 01:28:14 -0400

    may entries

---
+++ May 2024/ReplaceAllDots.js
@@ -0,0 +1,12 @@
+/* The code provided is supposed replace all the dots . in the specified String str with dashes -
+
+But it's not working properly.
+Task
+
+Fix the bug so we can all go home early. */
+
+var replaceDots = function(str) {
+    return str.replace(/\./g, '-');
+  }
+
+  console.log(replaceDots("one.two.three"))
