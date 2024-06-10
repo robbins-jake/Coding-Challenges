a39f8a2b60378c62046ef747e91c014cf80715e8
Jake Robbins <robbinsjake.t@gmail.com>
2024-06-10 01:14:50 -0400

    june entries

---
+++ June 2024/SumOfMultiples.js
@@ -0,0 +1,28 @@
+/* Find the sum of all multiples of n below m
+Keep in Mind
+
+    n and m are natural numbers (positive integers)
+    m is excluded from the multiples
+
+Examples
+
+sumMul(2, 9)   ==> 2 + 4 + 6 + 8 = 20
+sumMul(3, 13)  ==> 3 + 6 + 9 + 12 = 30
+sumMul(4, 123) ==> 4 + 8 + 12 + ... = 1860
+sumMul(4, -7)  ==> "INVALID" */
+
+function sumMul(n,m){
+    if(m < 0 || n * m === 0){
+        return 'INVALID'
+    }else{
+        let count= 0
+        for(let i = 0; i < m; i++){
+            if(i % n === 0){ 
+                count += i
+            }
+        }
+        return count
+    }
+}
+
+console.log(sumMul(2,9))
