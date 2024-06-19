496c2609a42ca6e5e7adc49df86957ceb32f8f5e
Jake Robbins <robbinsjake.t@gmail.com>
2024-06-19 01:11:46 -0400

    june entries

---
+++ June 2024/TripToMarket.js
@@ -0,0 +1,9 @@
+// There are only 2 guaranteed ways to tell if you are speaking to The Loch Ness Monster: A) it is a 400 foot tall beast from the paleolithic era; B) it will ask you for tree fiddy.
+
+// Since Nessie is a master of disguise, the only way accurately tell is to look for the phrase "tree fiddy". Since you are tired of being grifted by this monster, the time has come to code a solution for finding The Loch Ness Monster. Note that the phrase can also be written as "3.50" or "three fifty".
+
+function isLockNessMonster(s) {
+    return s.includes('tree fiddy') || s.includes('$3.50') || s.includes('three fifty')
+  }
+
+  console.log(isLockNessMonster("Your girlscout cookies are ready to ship. Your total comes to tree fiddy"))
