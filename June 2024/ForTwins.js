848a1c4de7b1c0cdb796601d89ac82a76ebe25a1
Jake Robbins <robbinsjake.t@gmail.com>
2024-06-25 01:09:12 -0400

    june entries

---
+++ June 2024/ForTwins.js
@@ -0,0 +1,7 @@
+//Write a function that will accept two parameters: variable and type and check if type of variable is matching type. Return true if types match or false if not.
+
+function typeValidation(variable, type) {
+    return type === typeof variable ? true : false
+}
+
+console.log(typeValidation('42', "string"))
