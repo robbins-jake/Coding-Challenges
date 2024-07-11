13f88de5b01b1706213b8153d5d8cd77e2d61f86
Jake Robbins <robbinsjake.t@gmail.com>
2024-07-11 00:54:21 -0400

    july entries

---
+++ July 2024/TranspoVaca.js
@@ -0,0 +1,15 @@
+// You will need a rental car in order for you to get around in your vacation. The manager of the car rental makes you some good offers.
+
+// Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
+
+function rentalCarCost(d) {
+    if(d >= 7){
+        return (d * 40) -50
+    }else if(d >= 3 && d < 7){
+        return (d * 40) - 20
+    }else{
+        return d * 40
+    }
+}
+
+console.log(rentalCarCost(7))
