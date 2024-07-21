dfc41ada7f8778a07cdefd081807e27e7b2b2f65
Jake Robbins <robbinsjake.t@gmail.com>
2024-07-21 00:50:05 -0400

    july entries

---
+++ July 2024/DivisibleByXY.js
@@ -0,0 +1,7 @@
+// Create a function that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero numbers.
+
+function isDivisible(n, x, y) {
+    return n % x === 0 && n % y === 0 ? true : false
+}
+
+console.log(isDivisible(3, 1, 3))
