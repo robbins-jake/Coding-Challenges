5d60806f2c3532b54b1e025e6a61f83e24d4b0ac
Jake Robbins <robbinsjake.t@gmail.com>
2024-08-10 00:36:22 -0400

    aug entries

---
+++ August 2024/SwitchItUp.js
@@ -0,0 +1,44 @@
+// When provided with a number between 0-9, return it in words.
+
+// Input :: 1
+
+// Output :: "One".
+
+function switchItUp(number){
+    switch(number){
+        case 0:
+            result = 'Zero'
+            break;
+        case 1:
+            result = 'One'
+            break;
+        case 2:
+            result = 'Two'
+            break;
+        case 3:
+            result = 'Three'
+            break;
+        case 4:
+            result = 'Four'
+            break;
+        case 5:
+            result = 'Five'
+            break;
+        case 6:
+            result = 'Six'
+            break;
+        case 7:
+            result = 'Seven'
+            break;
+        case 8:
+            result = 'Eight'
+            break;
+        case 9:
+            result = 'Nine'
+            break;
+        default:
+    }
+    return result
+}
+
+console.log(switchItUp(0))
