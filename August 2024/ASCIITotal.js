9c307d6b31d6d3ca88a80a9c7f1e9551972986e9
Jake Robbins <robbinsjake.t@gmail.com>
2024-08-25 00:29:20 -0400

    aug entries

---
+++ August 2024/ASCIITotal.js
@@ -0,0 +1,15 @@
+/* You'll be given a string, and have to return the sum of all characters as an int. The function should be able to handle all ASCII characters.
+
+examples:
+
+uniTotal("a") == 97 uniTotal("aaa") == 291 */
+
+function uniTotal (string) {
+    let count = 0
+    for(let i = 0; i < string.length; i++){
+        count += string.charCodeAt(i)
+    }
+    return count
+}
+
+console.log(uniTotal('aaa'))
