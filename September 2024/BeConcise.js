121e918970ec81643163206056cdebbf87e89f79
Jake Robbins <robbinsjake.t@gmail.com>
2024-09-17 00:16:27 -0400

    sept entries

---
+++ September 2024/BeConcise.js
@@ -0,0 +1,22 @@
+// You are given a function describeAge / describe_age that takes a parameter age (which will always be a positive integer) and does the following:
+
+//     If the age is 12 or lower, it return "You're a(n) kid"
+//     If the age is anything between 13 and 17 (inclusive), it return "You're a(n) teenager"
+//     If the age is anything between 18 and 64 (inclusive), it return "You're a(n) adult"
+//     If the age is 65 or above, it return "You're a(n) elderly"
+
+// Your task is to shorten the code as much as possible. Note that submitting the given code will not work because there is a character limit of 137.
+
+// I'll give you a few hints:
+
+//     The title itself is a hint - if you're not sure what to do, always research any terminology in this description that you have not heard of!
+//     Don't you think the whole "You're a(n) <insert_something_here>" is very repetitive? ;) Perhaps we can shorten it?
+//     Write everything in one line, \n and other whitespaces counts.
+
+function describeAge(age) {
+    let w="You're a(n) "
+    return age<13?`${w}kid`:age<18?`${w}teenager`:age<65?`${w}adult`:`${w}elderly`
+}
+
+console.log(describeAge(16))
+
