62a5ced64a67f83a4c43ba177a664494a32b971a
Jake Robbins <robbinsjake.t@gmail.com>
2024-10-13 00:04:23 -0400

    oct entries

---
+++ October 2024/StringObject.js
@@ -0,0 +1,9 @@
+// Implement a function which accepts 2 arguments: string and separator.
+
+// The expected algorithm: split the string into words by spaces, split each word into separate characters and join them back with the specified separator, join all the resulting "words" back into a sentence with spaces.
+
+function splitAndMerge(string, separator) {
+    return string.split(' ').map(a => a.split('').join(separator)).join(' ')
+}
+
+console.log(splitAndMerge("My name is John","-"))
