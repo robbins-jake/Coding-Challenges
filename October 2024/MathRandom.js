74eff0a3996abd948d1403fd0de94c990279ca3c
Jake Robbins <robbinsjake.t@gmail.com>
2024-10-29 23:56:47 -0400

    oct entries

---
+++ October 2024/MathRandom.js
@@ -0,0 +1,12 @@
+//This code is a mess! Would you help Pac to fix the code in time?
+
+function yourFutureCareer(){
+	var career = Math.random()
+	if (career <= 0.32) {
+		return 'FrontEnd Developer'
+    }else if (career <= 0.65){ 
+		return 'BackEnd Developer'
+	}else{
+		return 'Full-Stack Developer'
+	}
+}
